ee7f0ced4f1dbfb148f9aecdb4b018c8dd3b42b9
Sajid U. Abir <sajidabir01@gmail.com>
2022-09-11 00:01:54 +0600

    Initial commit

---
+++ js/tailwind.config.js
@@ -0,0 +1,18 @@
+tailwind.config = {
+    theme: {
+      extend: {
+        colors: {
+          'dark' : '#203239',
+          'orange': '#FF7527',
+          'yellow' : '#EEEDDE',
+          'gray' : '#2032398C'
+        },
+        fontFamily: {
+            'Poppins': ['Poppins'],
+          },
+        maxWidth: {
+            'container': '1166px',
+        }
+      }
+    }
+  }
