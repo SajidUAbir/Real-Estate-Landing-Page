c28909414bc80b80941b884babdd51350a435109
urme-akter <112346871+urme-akter@users.noreply.github.com>
2022-09-12 19:37:37 +0600

    residen section added

--- js/tailwind.config.js
+++ js/tailwind.config.js
@@ -1,18 +1,19 @@
 tailwind.config = {
-    theme: {
-      extend: {
-        colors: {
-          'dark' : '#203239',
-          'orange': '#FF7527',
-          'yellow' : '#EEEDDE',
-          'gray' : '#2032398C'
-        },
-        fontFamily: {
-            'Poppins': ['Poppins'],
-          },
-        maxWidth: {
-            'container': '1166px',
-        }
-      }
-    }
-  }
+  theme: {
+    extend: {
+      colors: {
+        dark: "#203239",
+        orange: "#FF7527",
+        yellow: "#EEEDDE",
+        gray: "#2032398C",
+        light: "#D6D5C7",
+      },
+      fontFamily: {
+        Poppins: ["Poppins"],
+      },
+      maxWidth: {
+        container: "1166px",
+      },
+    },
+  },
+};
